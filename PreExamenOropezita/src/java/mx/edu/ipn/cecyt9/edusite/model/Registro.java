3a4a4de03754a2633702ae70ad23d9a3aeee3cbc
Fernand0Barron <42643538+Fernand0Barron@users.noreply.github.com>
2018-10-08 08:53:12 -0500

    Add files via upload

---
+++ PreExamenOropezita/src/java/mx/edu/ipn/cecyt9/edusite/model/Registro.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ipn.cecyt9.edusite.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Alumno
+ */
+@Entity
+@Table(name = "registro")
+@NamedQueries({
+    @NamedQuery(name = "Registro.findAll", query = "SELECT r FROM Registro r")})
+public class Registro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_registro")
+    private Integer idRegistro;
+    @Size(max = 45)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 45)
+    @Column(name = "apellido")
+    private String apellido;
+
+    public Registro() {
+    }
+
+    public Registro(Integer idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    public Integer getIdRegistro() {
+        return idRegistro;
+    }
+
+    public void setIdRegistro(Integer idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idRegistro != null ? idRegistro.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Registro)) {
+            return false;
+        }
+        Registro other = (Registro) object;
+        if ((this.idRegistro == null && other.idRegistro != null) || (this.idRegistro != null && !this.idRegistro.equals(other.idRegistro))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.edu.ipn.cecyt9.edusite.model.Registro[ idRegistro=" + idRegistro + " ]";
+    }
+    
+}
---
+++ PreExamenOropezita/src/java/mx/edu/ipn/cecyt9/edusite/model/Rol.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ipn.cecyt9.edusite.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Alumno
+ */
+@Entity
+@Table(name = "rol")
+@NamedQueries({
+    @NamedQuery(name = "Rol.findAll", query = "SELECT r FROM Rol r")})
+public class Rol implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_rol")
+    private Integer idRol;
+    @Size(max = 60)
+    @Column(name = "decripcion")
+    private String decripcion;
+    @Column(name = "idResgistro")
+    private Integer idResgistro;
+
+    public Rol() {
+    }
+
+    public Rol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getDecripcion() {
+        return decripcion;
+    }
+
+    public void setDecripcion(String decripcion) {
+        this.decripcion = decripcion;
+    }
+
+    public Integer getIdResgistro() {
+        return idResgistro;
+    }
+
+    public void setIdResgistro(Integer idResgistro) {
+        this.idResgistro = idResgistro;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idRol != null ? idRol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Rol)) {
+            return false;
+        }
+        Rol other = (Rol) object;
+        if ((this.idRol == null && other.idRol != null) || (this.idRol != null && !this.idRol.equals(other.idRol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.edu.ipn.cecyt9.edusite.model.Rol[ idRol=" + idRol + " ]";
+    }
+    
+}
